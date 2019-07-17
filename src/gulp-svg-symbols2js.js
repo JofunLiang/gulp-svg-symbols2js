92b50df9459316fe386b9be6db855a10b95f111c
jofunliang <lianghaozhuo@yeah.net>
2019-07-18 00:18:42 +0800

    优化wrapper，增加stream报错

--- src/gulp-svg-symbols2js.js
+++ src/gulp-svg-symbols2js.js
@@ -4,6 +4,8 @@ var through = require('through2');
 var replaceExt = require('replace-ext');
 var escapeStr = require('js-string-escape');
 
+var PLUGIN_NAME = 'gulp-svg-symbols2js';
+
 function bufferToString (buffer, encoding) {
   var str;
   
@@ -21,13 +23,21 @@ module.exports = function () {
   return through.obj(function (file, enc, cb) {
     var isSVG = path.extname(file.path) === '.svg';
 
-    if (isSVG &&　file.isBuffer()) {
-      var wrapper = fs.readFileSync(path.join(__dirname, './wrapper.js'), 'utf8');
-      var svg = bufferToString(file.contents, enc);
-      var content = wrapper.replace('$$$', svg);
+    if (isSVG) {
+      // buffer
+      if (file.isBuffer()) {
+        var wrapper = fs.readFileSync(path.join(__dirname, './wrapper.js'), 'utf8');
+        var svg = bufferToString(file.contents, enc);
+        var content = wrapper.replace('$$$', svg);
+        
+        file.contents = new Buffer(content, 'utf8');
+        file.path = replaceExt(file.path, '.js');
+      }
       
-      file.contents = new Buffer(content, 'utf8');
-      file.path = replaceExt(file.path, '.js');
+      // stream
+      if (file.isStream()) {
+        throw new PluginError(PLUGIN_NAME, 'Cannot do SVG symbols to js on a stream!');
+      }
     }
     
     cb(null, file);
